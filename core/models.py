02cfbbe74e6806ed3d5a44f40efafa64cd896688
Patrick4350 <pbofah1@gmail.com>
2024-08-14 05:39:26 -0500

    Upload Form

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
 User = get_user_model()
 
@@ -10,6 +12,20 @@ class Profile(models.Model):
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField(max_length=150, default='First Name')
+    last_name = models.CharField(max_length=150, default='Last Name')
+    email = models.EmailField(default='example@example.com')
 
     def __str__(self):
         return self.user.username
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
--- core/views.py
+++ core/views.py
@@ -3,12 +3,18 @@ from django.contrib.auth.models import User
 from django.contrib import messages, auth
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Post
 
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render (request, 'index.html', {'user_profile': user_profile})
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1> Upload View </h1>')
 
 def signup(request):
     if request.method == 'POST':
@@ -28,20 +34,72 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                # Log user in and redirect to the home page
-                auth.login(request, user)
-                return redirect('index')  # Redirect to a page like 'index' or another appropriate page
+                #Log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
-                # Create a Profile object for the new user
-                Profile.objects.create(user=user, id_user=user.id)
 
+                #Create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
         else:
             messages.info(request, 'The passwords you entered do not match. Please try again.')
             return redirect('signup')
+
+
     else:
-        return render(request, 'signup.html')
+        return render (request, 'signup.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('images') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.first_name = first_name
+            user_profile.last_name = last_name
+            user_profile.location = location 
+            user_profile.email = email
+            user_profile.save()
+        
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.first_name = first_name
+            user_profile.last_name = last_name
+            user_profile.location = location 
+            user_profile.email = email
+            user_profile.save()
+        
+        return redirect('settings')
+
+
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 def signin(request):
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -50,10 +108,10 @@ def signin(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('index')  # Redirect to home page or dashboard
+            return redirect('/')
         else:
             messages.info(request, 'Invalid Credentials.')
-            return redirect('signin')
+            return redirect('/signin')
     else:
         return render(request, 'signin.html')
 
